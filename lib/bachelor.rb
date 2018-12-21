d0ac60885edab38db3a306e282716eecfcdc9389
Simon Kong <simon.kong95@gmail.com>
2018-12-21 19:37:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,6 +16,13 @@ end#def
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, people|
+    people.each do |bio, info|
+      if bio["occupation"] == occupation
+        return bio["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
