7793557985c1a9cd2f263fa19beda383981b8f39
Simon Kong <simon.kong95@gmail.com>
2018-12-21 19:31:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,6 +4,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |which_season, people|
     if which_season == season
       people.each do |info, value|
+        if info["status"] == "Winner"
         return info
       end#people
     end#if
